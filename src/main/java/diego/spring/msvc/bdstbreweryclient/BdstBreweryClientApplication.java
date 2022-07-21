01fd1e7b3dc6b0db2c6365cc29990cb340e151ab
Diego Tapia Guajardo <DiegoTGJobs@gmail.com>
2022-07-21 12:24:00 -0400

    Initial Commit

---
+++ src/main/java/diego/spring/msvc/bdstbreweryclient/BdstBreweryClientApplication.java
@@ -0,0 +1,13 @@
+package diego.spring.msvc.bdstbreweryclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdstBreweryClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BdstBreweryClientApplication.class, args);
+    }
+
+}
